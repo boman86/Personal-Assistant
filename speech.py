c9d84be5e8f36af7f6b39a31f0c9827f9af4605f
rchiang <dipo233@gmail.com>
2015-09-24 05:32:46 +0000

    switch to wit api directly

--- speech.py
+++ speech.py
@@ -1,35 +1,17 @@
 import pyaudio, os, subprocess
-import speech_recognition as sr
-from os import system
+import wit
 
 def main():
     # Initialize API keys and speech recognition
     WIT_AI_KEY = "45SK2K6B7EUC6GOJY26N2REG7LRJF4BT"
-    r = sr.Recognizer()
-    r.pause_threshold = 0.5
-        
-    while True:
-        with sr.Microphone() as source:
-            audio = r.listen(source)
 
-        # Case on keyword "APOLLO"
-        try: 
-            # Try to recognize audio
-            user = r.recognize_wit(audio, key=WIT_AI_KEY)
-            
-            # DEBUGGING PURPOSES
+    wit.init()
 
-            if user.lower() == "banana":
-                # Simulate taking in a command by beeping
-                audio_file = "/Users/richard/Desktop/Repos/PersonalAssistant/sounds/initialization_beep.wav"
-                return_code = subprocess.call(["afplay", audio_file])
-                system('say what''s up?')
-                break
-        except:
-            print("");
-            # error debugging
+    response = wit.voice_query_auto(WIT_AI_KEY)
+    print('response: {}'.format(response))
 
-
+    wit.close()
+    
 if __name__ == '__main__':
         main()
 
