3a53107c091299c0bf2b25e1b904ed43841d1234
rchiang <dipo233@gmail.com>
2015-10-08 14:29:07 +0000

    text to speech implementation

---
+++ voice.py
@@ -0,0 +1,17 @@
+import pyttsx
+
+# A text-to-speech engine using pyttsx. 
+
+def voice(input):
+    engine = pyttsx.init()
+
+    volume = engine.getProperty('volume')
+    voices = engine.getProperty('voices')
+    rate = engine.getProperty('rate')
+    
+    engine.setProperty('volume', volume-0.5)
+    engine.setProperty('rate', rate-65)
+    engine.say(input)
+    
+    engine.runAndWait()
+
